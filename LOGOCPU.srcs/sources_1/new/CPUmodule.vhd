724c6214022f69732c620f258251415f3abfd97e
John Arild Lolland <JohnArild+github@protonmail.com>
2019-10-12 09:51:22 +0200

    New project created with Vivado

---
+++ LOGOCPU.srcs/sources_1/new/CPUmodule.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: John Arild Lolland
+-- 
+-- Create Date: 10/12/2019 09:49:33 AM
+-- Design Name: 
+-- Module Name: CPUmodule - Master
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity CPUmodule is
+--  Port ( );
+end CPUmodule;
+
+architecture Master of CPUmodule is
+
+begin
+
+
+end Master;
